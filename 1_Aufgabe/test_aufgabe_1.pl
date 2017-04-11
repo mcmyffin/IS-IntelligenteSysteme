cb43c1de4f562bee02501d6fdc61270fc291f70f
MrTorgue <andreas.berks@haw-hamburg.de>
2017-04-12 01:34:40 +0200

    Refactored test prototype.

--- 1_Aufgabe/test_aufgabe_1.pl
+++ 1_Aufgabe/test_aufgabe_1.pl
@@ -1,15 +1,23 @@
-% load_test_files(test_aufgabe_1).
-% run_tests.
+% execute tests with:
+% ?- consult('aufgabe_1'), consult('test_aufgabe_1'), run_tests.
 
 :- begin_tests(aufgabe_1).
 
-% Albert will nicht zur Leitung gehören, wenn Emma nicht Vorsitzende wird.
+
+
+% a) Albert will nicht zur Leitung gehören, wenn Emma nicht Vorsitzende wird.
 test(leitung, fail) :- leitung(Leitung),
-              vorsitzender(Vorsitzender, Leitung), Vorsitzender \= emma,
-              zur_leitung_gehoeren(albert, Leitung).
-test(leitung, forall) :- leitung(Leitung),
-              vorsitzender(emma, Leitung);
-              nicht_zur_leitung_gehoeren(albert, Leitung).
+                       not(vorsitzender(emma, Leitung)),
+                       zur_leitung_gehoeren(albert, Leitung).
+test(leitung) :- leitung(Leitung),
+                 (     vorsitzender(emma, Leitung);
+                       not(zur_leitung_gehoeren(albert, Leitung))     ).
+
+
+
+% b) ...
+test(leitung) :- true.
+
 
 
 :- end_tests(aufgabe_1).
