1ab9dbe1a9449736afc43210c0790341372e04e7
MrTorgue <andreas.berks@haw-hamburg.de>
2017-04-25 18:57:21 +0200

    Added negative tests.

--- 2_Aufgabe/test_aufgabe_2.pl
+++ 2_Aufgabe/test_aufgabe_2.pl
@@ -3,24 +3,24 @@
 
 :- begin_tests(aufgabe_2).
 
-test(solveSudoku) :-
-     sudoku(1, Sudoku), sudoku(2, SudokuSolution),
+checkEqual(SudokuName, SolutionName) :-
+     sudoku(SudokuName, Sudoku), sudoku(SolutionName, Solution),
      solveSudoku(Sudoku),
-     Sudoku = SudokuSolution.
+     Sudoku = Solution.
 
-test(solveSudoku) :-
-     sudoku(2, Sudoku), sudoku(2, SudokuSolution),
-     solveSudoku(Sudoku),
-     Sudoku = SudokuSolution.
+test(solveSudoku) :- checkEqual(sudoku_1, solution_1).
+test(solveSudoku) :- checkEqual(solution_1, solution_1).
+test(solveSudoku) :- checkEqual(sudoku_2, solution_2).
+test(solveSudoku) :- checkEqual(solution_2, solution_2).
 
-test(solveSudoku) :-
-     sudoku(3, Sudoku), sudoku(4, SudokuSolution),
-     solveSudoku(Sudoku),
-     Sudoku = SudokuSolution.
-
-test(solveSudoku) :-
-     sudoku(4, Sudoku), sudoku(4, SudokuSolution),
-     solveSudoku(Sudoku),
-     Sudoku = SudokuSolution.
+test(solveSudoku, fail) :- sudoku(sudoku_fail_length_1, Sudoku), solveSudoku(Sudoku).
+test(solveSudoku, fail) :- sudoku(sudoku_fail_length_2, Sudoku), solveSudoku(Sudoku).
+test(solveSudoku, fail) :- sudoku(sudoku_fail_length_3, Sudoku), solveSudoku(Sudoku).
+test(solveSudoku, fail) :- sudoku(sudoku_fail_length_4, Sudoku), solveSudoku(Sudoku).
+test(solveSudoku, fail) :- sudoku(sudoku_fail_domain_1, Sudoku), solveSudoku(Sudoku).
+test(solveSudoku, fail) :- sudoku(sudoku_fail_domain_2, Sudoku), solveSudoku(Sudoku).
+test(solveSudoku, fail) :- sudoku(sudoku_fail_row_1, Sudoku), solveSudoku(Sudoku).
+test(solveSudoku, fail) :- sudoku(sudoku_fail_col_1, Sudoku), solveSudoku(Sudoku).
+test(solveSudoku, fail) :- sudoku(sudoku_fail_block_1, Sudoku), solveSudoku(Sudoku).
 
 :- end_tests(aufgabe_2).
