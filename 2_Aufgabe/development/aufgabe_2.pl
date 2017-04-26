13fc65e763b5c4947327964f98ff1a6e72cab5b8
MrTorgue <andreas.berks@haw-hamburg.de>
2017-04-26 16:16:42 +0200

    dev: reverted commit.

--- 2_Aufgabe/development/aufgabe_2.pl
+++ 2_Aufgabe/development/aufgabe_2.pl
@@ -1,10 +1,21 @@
 sudoku(Number) :-
      sudoku(Number, Sudoku),
-     solveSudoku(Sudoku).
+     solveSudoku(Sudoku),
+     printSudoku(Sudoku).
 
 solveSudoku(Sudoku) :-
-     % check length
-	 Sudoku =
+     checkLength(Sudoku),
+     checkDomain(Sudoku),
+     checkRows(Sudoku),
+     checkCols(Sudoku),
+     checkBlocks(Sudoku).
+
+checkLength(Sudoku) :- length(Sudoku, 9), checkLengthInner(Sudoku).
+checkLengthInner([]).
+checkLengthInner([Row|Rest]) :- length(Row, 9), checkLengthInner(Rest).
+
+checkDomain(Sudoku) :-
+     Sudoku =
           [[R1C1, R1C2, R1C3, R1C4, R1C5, R1C6, R1C7, R1C8, R1C9],
            [R2C1, R2C2, R2C3, R2C4, R2C5, R2C6, R2C7, R2C8, R2C9],
            [R3C1, R3C2, R3C3, R3C4, R3C5, R3C6, R3C7, R3C8, R3C9],
@@ -14,20 +25,20 @@ solveSudoku(Sudoku) :-
            [R7C1, R7C2, R7C3, R7C4, R7C5, R7C6, R7C7, R7C8, R7C9],
            [R8C1, R8C2, R8C3, R8C4, R8C5, R8C6, R8C7, R8C8, R8C9],
            [R9C1, R9C2, R9C3, R9C4, R9C5, R9C6, R9C7, R9C8, R9C9]],
+     SudokuFlatted =
+          [R1C1, R1C2, R1C3, R1C4, R1C5, R1C6, R1C7, R1C8, R1C9,
+           R2C1, R2C2, R2C3, R2C4, R2C5, R2C6, R2C7, R2C8, R2C9,
+           R3C1, R3C2, R3C3, R3C4, R3C5, R3C6, R3C7, R3C8, R3C9,
+           R4C1, R4C2, R4C3, R4C4, R4C5, R4C6, R4C7, R4C8, R4C9,
+           R5C1, R5C2, R5C3, R5C4, R5C5, R5C6, R5C7, R5C8, R5C9,
+           R6C1, R6C2, R6C3, R6C4, R6C5, R6C6, R6C7, R6C8, R6C9,
+           R7C1, R7C2, R7C3, R7C4, R7C5, R7C6, R7C7, R7C8, R7C9,
+           R8C1, R8C2, R8C3, R8C4, R8C5, R8C6, R8C7, R8C8, R8C9,
+           R9C1, R9C2, R9C3, R9C4, R9C5, R9C6, R9C7, R9C8, R9C9],
+     SudokuFlatted ins 1..9.
 
-     % check domain
+checkRows(Sudoku) :-
      Sudoku = [Row1, Row2, Row3, Row4, Row5, Row6, Row7, Row8, Row9],
-     Row1 ins 1..9,
-     Row2 ins 1..9,
-     Row3 ins 1..9,
-     Row4 ins 1..9,
-     Row5 ins 1..9,
-     Row6 ins 1..9,
-     Row7 ins 1..9,
-     Row8 ins 1..9,
-     Row9 ins 1..9,
-     
-     % check rows
      all_distinct(Row1),
      all_distinct(Row2),
      all_distinct(Row3),
@@ -36,31 +47,56 @@ solveSudoku(Sudoku) :-
      all_distinct(Row6),
      all_distinct(Row7),
      all_distinct(Row8),
-     all_distinct(Row9),
+     all_distinct(Row9).
 
-     % check cols
-     all_distinct([R1C1, R2C1, R3C1, R4C1, R5C1, R6C1, R7C1, R8C1, R9C1]),
-     all_distinct([R1C2, R2C2, R3C2, R4C2, R5C2, R6C2, R7C2, R8C2, R9C2]),
-     all_distinct([R1C3, R2C3, R3C3, R4C3, R5C3, R6C3, R7C3, R8C3, R9C3]),
-     all_distinct([R1C4, R2C4, R3C4, R4C4, R5C4, R6C4, R7C4, R8C4, R9C4]),
-     all_distinct([R1C5, R2C5, R3C5, R4C5, R5C5, R6C5, R7C5, R8C5, R9C5]),
-     all_distinct([R1C6, R2C6, R3C6, R4C6, R5C6, R6C6, R7C6, R8C6, R9C6]),
-     all_distinct([R1C7, R2C7, R3C7, R4C7, R5C7, R6C7, R7C7, R8C7, R9C7]),
-     all_distinct([R1C8, R2C8, R3C8, R4C8, R5C8, R6C8, R7C8, R8C8, R9C8]),
-     all_distinct([R1C9, R2C9, R3C9, R4C9, R5C9, R6C9, R7C9, R8C9, R9C9]),
+checkCols(Sudoku) :-
+     Sudoku =
+          [[R1C1, R1C2, R1C3, R1C4, R1C5, R1C6, R1C7, R1C8, R1C9],
+           [R2C1, R2C2, R2C3, R2C4, R2C5, R2C6, R2C7, R2C8, R2C9],
+           [R3C1, R3C2, R3C3, R3C4, R3C5, R3C6, R3C7, R3C8, R3C9],
+           [R4C1, R4C2, R4C3, R4C4, R4C5, R4C6, R4C7, R4C8, R4C9],
+           [R5C1, R5C2, R5C3, R5C4, R5C5, R5C6, R5C7, R5C8, R5C9],
+           [R6C1, R6C2, R6C3, R6C4, R6C5, R6C6, R6C7, R6C8, R6C9],
+           [R7C1, R7C2, R7C3, R7C4, R7C5, R7C6, R7C7, R7C8, R7C9],
+           [R8C1, R8C2, R8C3, R8C4, R8C5, R8C6, R8C7, R8C8, R8C9],
+           [R9C1, R9C2, R9C3, R9C4, R9C5, R9C6, R9C7, R9C8, R9C9]],
+     SudokuTransposed =
+          [[R1C1, R2C1, R3C1, R4C1, R5C1, R6C1, R7C1, R8C1, R9C1],
+           [R1C2, R2C2, R3C2, R4C2, R5C2, R6C2, R7C2, R8C2, R9C2],
+           [R1C3, R2C3, R3C3, R4C3, R5C3, R6C3, R7C3, R8C3, R9C3],
+           [R1C4, R2C4, R3C4, R4C4, R5C4, R6C4, R7C4, R8C4, R9C4],
+           [R1C5, R2C5, R3C5, R4C5, R5C5, R6C5, R7C5, R8C5, R9C5],
+           [R1C6, R2C6, R3C6, R4C6, R5C6, R6C6, R7C6, R8C6, R9C6],
+           [R1C7, R2C7, R3C7, R4C7, R5C7, R6C7, R7C7, R8C7, R9C7],
+           [R1C8, R2C8, R3C8, R4C8, R5C8, R6C8, R7C8, R8C8, R9C8],
+           [R1C9, R2C9, R3C9, R4C9, R5C9, R6C9, R7C9, R8C9, R9C9]],
+     checkRows(SudokuTransposed).
 
-     % check blocks
-     all_distinct([R1C1, R1C2, R1C3, R2C1, R2C2, R2C3, R3C1, R3C2, R3C3]),
-     all_distinct([R1C4, R1C5, R1C6, R2C4, R2C5, R2C6, R3C4, R3C5, R3C6]),
-     all_distinct([R1C7, R1C8, R1C9, R2C7, R2C8, R2C9, R3C7, R3C8, R3C9]),
-     all_distinct([R4C1, R4C2, R4C3, R5C1, R5C2, R5C3, R6C1, R6C2, R6C3]),
-     all_distinct([R4C4, R4C5, R4C6, R5C4, R5C5, R5C6, R6C4, R6C5, R6C6]),
-     all_distinct([R4C7, R4C8, R4C9, R5C7, R5C8, R5C9, R6C7, R6C8, R6C9]),
-     all_distinct([R7C1, R7C2, R7C3, R8C1, R8C2, R8C3, R9C1, R9C2, R9C3]),
-     all_distinct([R7C4, R7C5, R7C6, R8C4, R8C5, R8C6, R9C4, R9C5, R9C6]),
-     all_distinct([R7C7, R7C8, R7C9, R8C7, R8C8, R8C9, R9C7, R9C8, R9C9]),
+checkBlocks(Sudoku) :-
+     Sudoku =
+          [[R1C1, R1C2, R1C3, R1C4, R1C5, R1C6, R1C7, R1C8, R1C9],
+           [R2C1, R2C2, R2C3, R2C4, R2C5, R2C6, R2C7, R2C8, R2C9],
+           [R3C1, R3C2, R3C3, R3C4, R3C5, R3C6, R3C7, R3C8, R3C9],
+           [R4C1, R4C2, R4C3, R4C4, R4C5, R4C6, R4C7, R4C8, R4C9],
+           [R5C1, R5C2, R5C3, R5C4, R5C5, R5C6, R5C7, R5C8, R5C9],
+           [R6C1, R6C2, R6C3, R6C4, R6C5, R6C6, R6C7, R6C8, R6C9],
+           [R7C1, R7C2, R7C3, R7C4, R7C5, R7C6, R7C7, R7C8, R7C9],
+           [R8C1, R8C2, R8C3, R8C4, R8C5, R8C6, R8C7, R8C8, R8C9],
+           [R9C1, R9C2, R9C3, R9C4, R9C5, R9C6, R9C7, R9C8, R9C9]],
+     SudokuBlock =
+          [[R1C1, R1C2, R1C3, R2C1, R2C2, R2C3, R3C1, R3C2, R3C3],
+           [R1C4, R1C5, R1C6, R2C4, R2C5, R2C6, R3C4, R3C5, R3C6],
+           [R1C7, R1C8, R1C9, R2C7, R2C8, R2C9, R3C7, R3C8, R3C9],
+           [R4C1, R4C2, R4C3, R5C1, R5C2, R5C3, R6C1, R6C2, R6C3],
+           [R4C4, R4C5, R4C6, R5C4, R5C5, R5C6, R6C4, R6C5, R6C6],
+           [R4C7, R4C8, R4C9, R5C7, R5C8, R5C9, R6C7, R6C8, R6C9],
+           [R7C1, R7C2, R7C3, R8C1, R8C2, R8C3, R9C1, R9C2, R9C3],
+           [R7C4, R7C5, R7C6, R8C4, R8C5, R8C6, R9C4, R9C5, R9C6],
+           [R7C7, R7C8, R7C9, R8C7, R8C8, R8C9, R9C7, R9C8, R9C9]],
+     checkRows(SudokuBlock).
 
-     % print sudoku
+printSudoku(Sudoku) :-
+     Sudoku = [Row1, Row2, Row3, Row4, Row5, Row6, Row7, Row8, Row9],
      writeln(Row1),
      writeln(Row2),
      writeln(Row3),
